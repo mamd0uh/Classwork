73bacd6631b476c51629270a7444e38357a3a29f
mamd0uh <mamdouh.elmalah@gmail.com>
2022-08-10 16:02:49 -0400

    Classwork for '8-10'

---
+++ PracticeJS/app.js
@@ -0,0 +1,132 @@
+// //Exercise 1: 
+
+// let num1= 6; 
+// let num2= 4;
+// num3 = num1 + num2
+
+// //Exercise 2
+
+//  let num4= 9;
+//  let num5= 8;
+
+//  num6= num4*num5
+//  num7= num4-num1
+
+// //Exercise 3
+
+// let x1=1;
+// let x2=2;
+// let x3=3;
+// let x4=4;
+// let x5=5;
+// let x6=6;
+// let x7=7;
+// let x8=8;
+// let x9=9;
+// let x10=10;
+
+// average1=((x1+x2+x3+x4+x5)/5)
+// //Average of first 5 numbers
+// average2=((x6+x7+x8+x9+x10)/5)
+// //Average of last 5 numbers
+// average3=((average1+average2)/2)
+// //Average of above two results
+
+// //Exercise 4
+
+
+// let y1=4;
+// let y2=10;
+// let y3=22;
+// let y4=-30;
+// let y5=55;
+// let y6=68;
+// let y7=7;
+// let y8=-22;
+// let y9=9;
+// let y10=100;
+
+// average4=((y1+y2+y3+y4+y5)/5)
+// //Average of first 5 numbers
+// average5=((y6+y7+y8+y9+y10)/5)
+// //Average of last 5 numbers
+// average6=((average1+average2)/2)
+// //Average of above two results
+
+// console.log(average6)
+
+// let box1='Hello';
+// let box2='Class';
+// let box3
+// console.log(box2, box1)
+
+// box3=box1
+// box1=box2
+// box2=box3
+
+// console.log(box2, box1)
+
+//Exercise (1)
+
+
+// let number=7
+
+// if (number > 0) {
+//     console.log('Number is posistive')
+// } else {
+
+//     console.log('Number is negative')
+// }
+
+// // Exercise (2)
+
+// let age= 15; 
+// if (age>=18) {
+
+//     console.log("Welcome to the site")
+// }
+// else {
+//     console.log('Access Denied')
+// }
+
+// let num=-10;
+
+// if (num>100) {
+//     console.log('Number is positive & greater than 100');
+// } else if (num<=100 && num>0) {
+//     console.log('Number is positive but less than 100');
+// } else {
+//     console.log('Number is Negative');
+// }
+
+// let grade = 67;
+
+// if (grade >= 55) {
+//     if (grade >= 90) {
+//         console.log('A');
+//     } else if (grade >= 80) {
+//         console.log('B');
+//     } else if (grade >= 70) {
+//         console.log('C');
+//     } else {
+//         console.log('D');
+//     }
+// } else {
+//     console.log('F');
+// }
+
+let letter = "A"; 
+
+switch(letter){
+
+    case "A":
+    case "E":
+    case "I":
+    case "O":
+    case "U":
+        console.log('Letter is Vowel')
+    break;
+    default: 
+        console.log('Letter is consonant')
+    break;
+}
