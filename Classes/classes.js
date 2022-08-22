7191d357fb6423991dd75c7f1105df4f241454fc
mamd0uh <mamdouh.elmalah@gmail.com>
2022-08-22 15:57:40 -0400

    commit 8-22-2022

--- Classes/classes.js
+++ Classes/classes.js
@@ -58,57 +58,182 @@
 //     console.log(toyota.toString())
 
 
-class Character {
-    constructor (name, age, eyes, hair, lovesCats = true, lovesDogs) {
-      this.legs = 2;
-      this.arms = 2;
-      this.name = name;
-      this.age = age;
-      this.eyes = eyes;
-      this.hair = hair;
-      this.lovesCats = lovesCats;
-      this.lovesDogs = lovesDogs || false;  // short-circuit evaluation
-​
-      console.log('inside of Character constructor...')
-    }
-    greet (otherCharacter) {
-      console.log('hi ' + otherCharacter + '!');
-    }
-    classyGreeting (otherClassyCharacter) {
-      console.log('Howdy ' + otherClassyCharacter.name + '!');
-    }
-    setHair (hairColor) {
-      this.hair = hairColor;
-    }
-​
-    smite () {
-      console.log('i smited thee.');
-    }
+// class Character {
+//     constructor (name, age, eyes, hair, lovesCats = true, lovesDogs) {
+//       this.legs = 2;
+//       this.arms = 2;
+//       this.name = name;
+//       this.age = age;
+//       this.eyes = eyes;
+//       this.hair = hair;
+//       this.lovesCats = lovesCats;
+//       this.lovesDogs = lovesDogs || false;  // short-circuit evaluation
+// ​
+//       console.log('inside of Character constructor...')
+//     }
+//     greet (otherCharacter) {
+//       console.log('hi ' + otherCharacter + '!');
+//     }
+//     classyGreeting (otherClassyCharacter) {
+//       console.log('Howdy ' + otherClassyCharacter.name + '!');
+//     }
+//     setHair (hairColor) {
+//       this.hair = hairColor;
+//     }
+// ​
+//     smite () {
+//       console.log('i smited thee.');
+//     }
   
-  }
-​
-// child class and/or sub class
-// use extend to inherit properties and methods 
-// from parent and/or super class
-class Hobbit extends Character {
-    constructor(name, age, eyes, hair, height, weight) { // setup parameters...
-        super(name, age, eyes, hair) // pass them as arguments...
-        this.height = height
-        this.weight = weight
-        console.log('inside of Hobbit constructor...')
-    }
-    static steal() {
-        console.log('woohoo!')
-    }
-    greet(name) {
-        console.log('teeheee hi ' + name)
-        super.smite()
-​
+//   }
+// ​
+// // child class and/or sub class
+// // use extend to inherit properties and methods 
+// // from parent and/or super class
+// class Hobbit extends Character {
+//     constructor(name, age, eyes, hair, height, weight) { // setup parameters...
+//         super(name, age, eyes, hair) // pass them as arguments...
+//         this.height = height
+//         this.weight = weight
+//         console.log('inside of Hobbit constructor...')
+//     }
+//     static steal() {
+//         console.log('woohoo!')
+//     }
+//     greet(name) {
+//         console.log('teeheee hi ' + name)
+//         super.smite()
+// ​
         
-    }
+//     }
+// }
+// ​
+// const frodo = new Hobbit('Frodo', 30, 'brown', 'black', 10, 100)
+// // console.log(frodo)
+// // frodo.greet('Sam')
+// ​
+
+// class Person {
+
+// constructor(name) {
+
+// this.name = name; 
+// this.age = 0; 
+// this.height = 0;
+// this.weight = 0;
+// this.mood = 0;
+// this.hamster = [];
+// this.bankAccount = 0;
+
+// }
+
+// getName() {
+//   this.name = '';
+// }
+
+// getAge() {
+//   return this.age 
+// }
+
+// getWeight() {
+//   return this.weight 
+// }
+
+// getHeight() {
+//   return this.height 
+// }
+
+// greet(name) {
+//   console.log('Hello' + name)
+// }
+
+// eat() {
+//   return this.weight++, this.mood++
+// }
+
+// excerice() {
+//   return this.weight--
+// }
+
+// ageUp() {
+//   this.age ++;
+//   this.height ++;
+//   this.weight ++;
+//   this.mood --; 
+//   this.bankAccount += 10
+// }
+
+// buyHamster(hamster) {
+
+//   thisHamster.push(hamster);
+//   return this.mood += 10, this.bankAccount -= this.getPrice() 
+// }
+
+// }
+
+// const timmy = new Person ('Timmy');
+// timmy.ageUp()
+// timmy.ageUp()
+// timmy.ageUp()
+// timmy.ageUp()
+// timmy.ageUp()
+
+// timmy.eat()
+// timmy.eat()
+// timmy.eat()
+// timmy.eat()
+// timmy.eat()
+
+// timmy.excerice()
+// timmy.excerice()
+// timmy.excerice()
+// timmy.excerice()
+// timmy.excerice()
+
+// for (let i = 0; i < 9; i++){
+//   timmy.ageUp()
+// }
+
+// const hamster = new Hamster('Gus') //Check this one
+
+// hamster.owner = "Timmy"
+
+// timmy.buyHamster(hamster)
+
+// for (let i = 0; i < 15; i++){
+//   timmy.ageUp()
+// }
+
+// timmy.eat()
+// timmy.eat()
+
+// timmy.excerice()
+// timmy.excerice()
+
+
+class Dinner {
+
+constructor (appetizer, entree, dessert) {
+
+  this.appetizer = appetizer;
+  this.entree = entree; 
+  this.dessert = dessert; 
 }
-​
-const frodo = new Hobbit('Frodo', 30, 'brown', 'black', 10, 100)
-// console.log(frodo)
-// frodo.greet('Sam')
-​
+
+}
+
+class Chef { 
+
+createDinner(appetizer, entree, dessert) {
+  const newDinner = new Dinner (appetizer, entree, dessert);
+  return newDinner;
+}
+
+}
+
+
+const newChef = new Chef();
+const dinner = newChef.createDinner('French Fries', "Pizza", "Ice Cream")
+
+console.log(dinner);
+
--- Review/JSreview.js
+++ Review/JSreview.js
@@ -51,3 +51,94 @@
 // else if (age < 16){
 //     console.log("Sorry, you're too young.")
 // }
+
+// II. LOOPS
+
+//         // A. The basics
+
+// 1.
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// 2. 
+// for (let i = 10; i <= 400; i++) {
+//     console.log(i);
+// }
+
+// 3. 
+// for (let i = 12; i < 4001; i+=3) {
+//     console.log(i);
+// }
+
+//         // B. Get even
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i +"<-- is an even number");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// C1- C3
+// for (let i = 1; i <= 100; i++){
+//     if (i % 5 === 0 && i % 3 === 0){
+//         console.log("I found a " + i + " High five!")
+//         console.log("I found a " + i + " Three is a crowd.")
+//     }
+//     else if (i % 5 === 0){
+//         console.log("I found a " + i + " High five!")
+
+//     }
+//     else if(i % 3 === 0){
+//         console.log("I found a " + i + " Three is a crowd.")
+
+//     }
+  
+// }
+
+// D. Savings Account
+
+// 1.
+// const bank_account = () => {
+//     let sum = 0;
+//     for (let i = 1; i <= 10; i++) {
+//         sum += i;
+//     }
+//     return sum;
+// }
+// console.log(bank_account());
+
+// 2.
+// const bank_account = () => {
+//     let sum = 0;
+//     for (let i = 1; i <= 100; i++) {
+//         sum += i;
+//     }
+//     return sum * 2;
+// }
+// console.log(bank_account());
+
+        //Arrays & Control flow
+// A1-  Elements
+// A2-  Yes, in the order that you create
+// A3- Shopping list, Grocery list
+
+// B1- let quotes = [ 'Be yourself', 'Be practical', 'Love yourself']
+
+// C1- randomThings[0]
+// C2-C3 
+// const randomThings = [1, 10, 'Hello', true]
+// randomThings[2] = 'World'
+// console.log(randomThings)
+
+// const ourClass = ['Slaty', 'Zoom', 'Sardine', 'Slack', 'Github']
+// D1- ourClass[3]
+// D2- 
+// ourClass[4] = 'Octocat'
+// console.log(ourClass)
+
+// D3-
+//  ourClass.push('Cloud City')
+//  console.log(ourClass)
